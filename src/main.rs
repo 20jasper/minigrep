4ca4bbb83ee400c65b5df34cdcebf65c91c0ddfc
Jacob Asper <jacobasper191@gmail.com>
2023-09-02 19:20:59 -0400

    use parse_config function

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ use std::env;
 use std::fs;
 
 fn main() {
-    let args = env::args().collect::<Vec<String>>();
+	let args = env::args().collect::<Vec<String>>();
 
-    let query = &args[1];
-    let file_path = &args[2];
+	let (query, file_path) = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+	let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+	println!("With text:\n{contents}");
+}
 
-    println!("With text:\n{contents}");
+fn parse_config(args: &[String]) -> (&str, &str) {
+	let query = &args[1];
+	let file_path = &args[2];
+
+	println!("Searching for {}", query);
+	println!("In file {}", file_path);
+
+	(query, file_path)
 }
