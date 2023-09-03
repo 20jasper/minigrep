394cdcd9c215a5a9477d15ff8823989c0537383f
Jacob Asper <jacobasper191@gmail.com>
2023-09-02 20:21:13 -0400

    pull business logic out to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
 	let args = env::args().collect::<Vec<String>>();
 
-	// we care about the Ok variant, so we use unwrap_or_else to handle Err
+	// the Ok variant matters, so we use unwrap_or_else to handle Err
 	// variant or hold the value
 	let config = Config::build(&args).unwrap_or_else(|error| {
 		println!("Problem parsing arguments: {error}");
@@ -14,38 +14,10 @@ fn main() {
 		process::exit(1);
 	});
 
-	// we don't care about Ok variant, so use if let to throw away Ok variant and
+	// Ok variant doesn't matter, so use if let to throw away Ok variant and
 	// handle Err variant
-	if let Err(error) = run(config) {
+	if let Err(error) = minigrep::run(config) {
 		println!("Application error: {error}");
 		process::exit(1);
-	}
-}
-
-// returns unit when ok, and any error otherwise
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-	let contents = fs::read_to_string(config.file_path)?;
-
-	println!("With text:\n{contents}");
-
-	Ok(())
-}
-
-struct Config {
-	query: String,
-	file_path: String,
-}
-
-impl Config {
-	// `build` is more semantic here since `new` is expected never to fail
-	fn build(args: &[String]) -> Result<Config, &'static str> {
-		if args.len() < 3 {
-			return Err("not enough arguments");
-		}
-
-		let query = args[1].clone();
-		let file_path = args[2].clone();
-
-		Ok(Config { query, file_path })
 	}
 }
